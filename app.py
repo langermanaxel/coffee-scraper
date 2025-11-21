e696b9d4792c29fdd8b80e4661847966ecb81c90
Axel Langerman <axellangerman@yahoo.com>
2025-11-21 16:38:16 -0300

    render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from api.routes import api_bp
 from web.routes import web_bp
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ app.register_blueprint(api_bp)
 app.register_blueprint(web_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
